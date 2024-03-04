a33a24071c39a0e81f89586047c8e67b59af64d4
Shim Jae Chun <shimnaldo98@gmail.com>
2024-03-04 12:16:36 +0900

    Feat : add JsonData

--- Assets/02. Script/Data/DataController.cs
+++ Assets/02. Script/Data/DataController.cs
@@ -11,14 +11,17 @@ public class DataController : MonoBehaviour
     private string path => $"{Application.persistentDataPath}/Data"
 #endif
 
+    public GameData gameData;
+
     [ContextMenu("Save")]
     public void Save()
     {
         if (!File.Exists(path))
             Directory.CreateDirectory(path);
 
-        string filepath = Path.Combine(Application.persistentDataPath, "Data/Test.txt");
-        File.WriteAllText(filepath, "Saved Data");
+        string filepath = Path.Combine(path, "Test.txt");
+        string json = JsonUtility.ToJson(gameData, true);
+        File.WriteAllText(filepath, json);
     }
 
     [ContextMenu("Load")]
@@ -26,7 +29,10 @@ public class DataController : MonoBehaviour
     {
         string filePath = Path.Combine(Application.dataPath, "Data/Test.txt");
         if (File.Exists(filePath))
-            File.ReadAllText(filePath);
+        {
+            string json = File.ReadAllText(filePath);
+            gameData = JsonUtility.FromJson<GameData>(json);
+        }
         else
             Debug.Log($"File path : {filePath} does not exist");
     }
---
+++ Assets/02. Script/Data/GameData.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class GameData
+{
+    public int level;
+    public int atk;
+    public int amr;
+}
